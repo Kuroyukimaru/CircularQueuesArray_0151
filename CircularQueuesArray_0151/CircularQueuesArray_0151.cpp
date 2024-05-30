ba0a14acf023eb4b13bc36d19ee892e3a339cce4
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-30 11:11:29 +0700

    jika FRONT <- REAR, iterasi dari FRONT hingga REAR

--- CircularQueuesArray_0151/CircularQueuesArray_0151.cpp
+++ CircularQueuesArray_0151/CircularQueuesArray_0151.cpp
@@ -59,7 +59,24 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+    void display() {
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
 
+        cout << "\nElements in the queue are...\n";
+
+        //jika FRONT <- REAR, iterasi dari FRONT hingga REAR
+        if (FRONT <= REAR) {
+            while (FRONT <= REAR) {
+                cout << queue_array[FRONT] << " ";
+                FRONT++;
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main()
